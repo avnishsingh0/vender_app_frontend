06a45fd8181f116cfda28db3908271c14df4de9d
avnish <avnishbdp@gmail.com>
2023-08-24 15:34:03 +0530

    api-chenge

--- src/server.js
+++ src/server.js
@@ -1,2 +1,2 @@
-export const server ="https://venderappbackend-production.up.railway.app/api/v2";
-export const backend_url ="https://venderappbackend-production.up.railway.app/";
+export const server ="https://erin-proud-yak.cyclic.cloud/api/v2";
+export const backend_url ="https://erin-proud-yak.cyclic.cloud/";
